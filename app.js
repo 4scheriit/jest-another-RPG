80aeb3b6fd4dbfe5f1fa240f0fa50912f789bb48
Maxwell Brown <mbrownet@gmail.com>
2023-05-31 14:15:16 -0400

    Implimented the battle function to play the game

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ import initializeGame from "./lib/Game.js";
 
 async function runGame() {
   const game = await initializeGame();
-  console.log("Enemy", game.currentEnemy);
-  console.log("Player", game.player);
-  console.log("Game initialized");
 }
 
 runGame();
